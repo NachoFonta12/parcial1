a031161af69709d6bcb54cf4c95a8de715d5d168
Nasho <NachoFonta12@gmail.com>
2026-05-18 21:44:05 -0300

    Ruta a home arreglada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { WhoIAm } from './components/who-i-am/who-i-am';
 export const routes: Routes = [
     {path: 'login', component: Login},
     {path: 'register', component: Register},
+    {path: 'home', component: Home},
     {path: '', component: Home},
     {path: 'about-me', component: WhoIAm},
     {path: '****', redirectTo: 'login'}

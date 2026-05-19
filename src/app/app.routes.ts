0b7ee9d2b16eb704b5eba62ea66877ec2393b85a
Nasho <NachoFonta12@gmail.com>
2026-05-19 18:24:11 -0300

    Guards funcionales, botones escondidos si el usuario no está registrado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { Login } from './components/login/login';
 import { Home } from './components/home/home';
 import { Register } from './components/register/register';
 import { WhoIAm } from './components/who-i-am/who-i-am';
+import { userLogged } from './guards/userLogged';
 
 export const routes: Routes = [
     {path: 'login', component: Login},
     {path: 'register', component: Register},
-    {path: 'home', component: Home},
-    {path: '', component: Home},
-    {path: 'about-me', component: WhoIAm},
+    {path: 'home', component: Home, canActivate: [userLogged]},
+    {path: '', component: Home, canActivate: [userLogged]},
+    {path: 'about-me', component: WhoIAm, canActivate: [userLogged]},
     {path: '****', redirectTo: 'login'}
 ];
